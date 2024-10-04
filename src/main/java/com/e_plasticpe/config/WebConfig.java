74b303bd4d54e3418d1a45be34ba4c24b5b2ad5f
pravinpimpale <pravinpimpale45@gmail.com>
2024-10-04 14:22:56 +0530

    changed

--- src/main/java/com/e_plasticpe/config/WebConfig.java
+++ src/main/java/com/e_plasticpe/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer{
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")  // Adjust this mapping to match your endpoints
+                registry.addMapping("/**")  // Adjust this mapping to match your endpoints
                         .allowedOrigins("http://localhost:3000")  // Add your React app URL here
                         .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS")  // Allow needed HTTP methods
                         .allowedHeaders("*")
